f67113dbae33ebeade3d1ae33db12670df4e83ea
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2020-12-27 21:42:28 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">nuxt-templete</h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  margin: 0 auto;
+  text-align: center;
+}
+
+.title {
+  display: block;
+  font-size: 100px;
+  font-weight: 300;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  padding-bottom: 15px;
+  font-weight: 300;
+  color: #fff;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ stylelint.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recess-order',
+    'stylelint-config-recommended-scss',
+    'stylelint-prettier/recommended',
+  ],
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {
+    'number-leading-zero': 'never',
+  },
+}
