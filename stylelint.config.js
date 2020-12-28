2ae703ac1a076dc26324261f16743d22a0e1e2b7
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2020-12-28 20:10:15 +0900

    fix lint setting

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,11 +3,9 @@ module.exports = {
     'stylelint-config-standard',
     'stylelint-config-recess-order',
     'stylelint-config-recommended-scss',
-    'stylelint-prettier/recommended',
+    // 'stylelint-prettier/recommended',
   ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {
-    'number-leading-zero': 'never',
-  },
+  rules: {},
 }
